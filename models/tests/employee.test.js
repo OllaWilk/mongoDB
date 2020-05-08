ed7e0bb62ee954ae84e71a127d0b1c0f254bc411
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-08 16:37:18 +0200

    test employee model

---
+++ models/tests/employee.test.js
@@ -0,0 +1,56 @@
+const Employee = require('../employee.model');
+const mongoose = require('mongoose');
+const expect = require('chai').expect;
+
+describe('Employee', () => {
+
+    it('should throw an error if no arg', () => {
+        const emp = new Employee({});
+
+        emp.validate(err => {
+          expect(err.errors.firstName).to.exist;
+          expect(err.errors.lastName).to.exist;
+          expect(err.errors.department).to.exist;
+        });
+    });
+
+    it('should throw an error if one of "arg" is missing ', () => {
+
+          const emp = new Employee({ firstName: 'Ola', lastName: 'Wilk' });
+
+          emp.validate(err => {
+            expect(err.errors.department).to.exist;
+          });
+
+    });
+
+    it('should throw an error if "arg" are not a string', () => {
+
+        const cases = [{}, []];
+        for(let name of cases) {
+          const emp = new Employee({ name });
+
+          emp.validate(err => {
+            expect(err.errors.firstName).to.exist;
+            expect(err.errors.lastName).to.exist;
+            expect(err.errors.department).to.exist;
+          });
+        }
+    });
+
+    it('should not throw an error if "arg" is okay', () => {
+
+        const emp = new Employee({ firstName: 'Ola', lastName: 'Wilk', department: 'Testing' });
+
+        emp.validate(err => {
+        expect(err).to.not.exist;
+        });
+
+    });
+
+
+    after(() => {
+        mongoose.models = {};
+    });
+
+});
