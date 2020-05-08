dbd3263af69c50a8ed52e6388caf84c8ee229e42
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-08 15:58:40 +0200

    test department

---
+++ models/tests/department.test.js
@@ -0,0 +1,59 @@
+const Department = require('../department.model.js');
+const mongoose = require('mongoose');
+const expect = require('chai').expect;
+
+describe('Department', () => {
+
+    it('should throw an error if no "name" arg', () => {
+        const dep = new Department({}); // create new Department, but don't set `name` attr value
+
+        dep.validate(err => {
+          expect(err.errors.name).to.exist;
+        });
+    });
+
+    it('should throw an error if "name" is not a string', () => {
+
+        const cases = [{}, []];
+        for(let name of cases) {
+          const dep = new Department({ name });
+
+          dep.validate(err => {
+            expect(err.errors.name).to.exist;
+          });
+        }
+    });
+
+    it('"name" should contein 5-20 letters', () => {
+
+        const cases = ['Abcd', 'abcde fghit klmnoprstuwx']; // a to jest bez sensu.. a co jak max liczba liter to 200?
+
+        for(let name of cases) {
+          const dep = new Department({ name });
+
+          dep.validate(err => {
+            expect(err.errors.name).to.exist;
+          });
+        }
+    });
+
+    it('should not throw an error if "name" is okay', () => {
+
+        const cases = ['Management', 'Human Resources'];
+        for(let name of cases) {
+          const dep = new Department({ name });
+
+          dep.validate(err => {
+            expect(err).to.not.exist;
+          });
+
+        }
+
+    });
+
+
+    after(() => {
+        mongoose.models = {};
+    });
+
+});
