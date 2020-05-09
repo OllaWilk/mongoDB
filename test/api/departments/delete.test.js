cea27b11c2fbc6d1ac21e6fef54a04179e37b8da
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-05-09 23:39:53 +0200

    prepere to test endpoints

---
+++ test/api/departments/delete.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
+const server = require('../../server.js');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('DELETE /api/departments', () => {
+
+});
+
---
+++ test/api/departments/post.test.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
+const server = require('../../server.js');
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe('POST /api/departments', () => {
+
+});
